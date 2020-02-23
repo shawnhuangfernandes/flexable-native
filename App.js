fe82ed5b3c39d82f41d7dac59a941a19a4278a82
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-02-22 22:41:39 -0800

    Added a React Stack Navigator to my app, will be playing around with settings

--- App.js
+++ App.js
@@ -1,27 +1,33 @@
 // React Native imports
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 // Custom imports
-import colors from './constants/colors'
-import LandingScreen from './screens/LandingScreen'
-import AuthenticateScreen from './screens/AuthenticateScreen'
-import MenuSelectionScreen from './screens/MenuSelectionScreen'
+import colors from "./constants/colors";
+import LandingScreen from "./screens/LandingScreen";
+import AuthenticateScreen from "./screens/AuthenticateScreen";
+import MenuSelectionScreen from "./screens/MenuSelectionScreen";
+
+import { YellowBox } from "react-native";
+YellowBox.ignoreWarnings(["Remote debugger"]);
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.screen}>
-          <MenuSelectionScreen/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="LandingScreen" component={LandingScreen} />
+        <Stack.Screen name="AuthenticateScreen" component={AuthenticateScreen} />
+        <Stack.Screen name="MenuSelectionScreen" component={MenuSelectionScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  screen: {
-    height: '100%',
-    width: '100%',
-    paddingTop: 40,
-    paddingHorizontal: 15,
-    backgroundColor: colors.primary
-  },
-});
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -2,14 +2,28 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native'
 
+// Custom imports
+import colors from "../constants/colors";
+
 const LandingScreen = () => {
     return (
         <View>
-            <Text>
+            <Text style={styles.screen}>
                 This is the landing screen
             </Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    screen: {
+      height: "100%",
+      width: "100%",
+      paddingTop: 40,
+      paddingHorizontal: 15,
+      backgroundColor: colors.primary
+    }
+  });
+  
 
 export default LandingScreen;

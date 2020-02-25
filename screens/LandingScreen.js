bb2f60f219ed9d4f24c44726ddfcdb579dfdcee5
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-02-24 22:23:46 -0800

    Added debugging background colors on Landing Screen to hold buttons, images, an gifs

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,6 +1,6 @@
 // React Native imports
 import React from "react";
-import { View, Button, Text, StyleSheet } from "react-native";
+import { View, Button, Image, StyleSheet } from "react-native";
 
 // Custom imports
 import colors from "../constants/colors";
@@ -8,11 +8,18 @@ import colors from "../constants/colors";
 const LandingScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
-      <Text>This is the landing screen</Text>
+      <View style={styles.header}>
+
+      </View>
+      <View style={styles.body}>
+        
+      </View>
+      <View style={styles.buttonContainer}>
       <Button
         title="To Login"
         onPress={() => navigation.navigate("AuthenticateScreen")}
       />
+      </View>
     </View>
   );
 };
@@ -21,10 +28,24 @@ const styles = StyleSheet.create({
   screen: {
     height: "100%",
     width: "100%",
-    paddingTop: 40,
+    paddingVertical: 40,
     borderWidth: 0,
     paddingHorizontal: 15,
     backgroundColor: colors.primary
+  },
+  header: {
+    backgroundColor: 'red',
+    height: '25%',
+  },
+  body: {
+    backgroundColor: 'green',
+    height: '50%',
+  },
+  buttonContainer: {
+    backgroundColor: 'blue',
+    height: '25%',
+    flexDirection: 'column',
+    justifyContent: 'center'
   }
 });
 
